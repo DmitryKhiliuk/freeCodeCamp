8d9a878f33a1d35425323fb200686e59a46b7962
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-01-11 20:30:16 +0300

    fCC

---
+++ script.js
@@ -0,0 +1,88 @@
+/* 1 */
+
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+/* 2 */
+/* Use the var keyword to create a variable called myName. */
+
+var myName;
+
+/* 3 */
+/* Assign the value 7 to variable a. */
+
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+
+/* 4 */
+
+/* Assign the contents of a to variable b. */
+
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+/* 5 */
+
+/* Define a variable a with var and initialize it to a value of 9. */
+
+var a = 9;
+
+/* 6 */
+
+/* Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+ */
+
+var myFirstName = "Dmitry";
+var myLastName = "Khiliuk";
+
+/* 7 */
+
+/* Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined. */
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/* 8 */
+
+/* Modify the existing declarations and assignments so their names use camelCase. */
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/* 9 */
+
+/* Update the code so it only uses the let keyword. */
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+/* 10 */
+
+/* Change the code so that all variables are declared using let or const. */
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
