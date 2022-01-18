82dc982f8069f34b3100e00c9fb7eb3c63ee8c42
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-01-18 18:35:27 +0300

    40

---
+++ task39.js
@@ -0,0 +1,4 @@
+/* 39 */
+/* Modify the new array myArray so that it contains both a string and a number (in that order). */
+// Only change code below this line
+const myArray = ["Hello", 10];
---
+++ task40.js
@@ -0,0 +1,4 @@
+/* 40 */
+/* Create a nested array called myArray. */
+// Only change code below this line
+const myArray = [[1, 2], [1, 2]];
