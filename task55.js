d3abab5bea4f0c4a7916ee1560047a809310482d
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-01-19 16:49:54 +0300

    55

---
+++ task55.js
@@ -0,0 +1,22 @@
+/* 55 */
+
+ /* Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined. */
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+function addFive() {
+  sum += 5;
+}
+
+// Only change code below this line
+
+
+// Only change code above this line
+
+addThree();
+addFive();
